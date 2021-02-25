10f443c4723375ad0e5578867901b34c106966b6
Serega-555 <21zozo@mail.ru>
2021-02-25 17:15:50 +0600

    Что то новое не знаю

--- tes.py
+++ tes.py
@@ -5,12 +5,15 @@ import math
 
 
 def square(n, pix):
-    # R = pix / (2 * math.sin(math.radians(360 / (2 * n))))
-    
-    t.goto(0,0)
+
     for j in range(n):
-        t.forward(pix)
-        t.left(360 / n)
+        R = pix / (2 * math.sin(math.radians(360 / (2 * n))))
+        t.penup()
+        t.goto(-R, -R)
+        for jj in range(3, n + 1, 1):
+            t.pendown()
+            t.forward(pix)
+            t.left(360 / jj)
         
 
 square(10, 40)
