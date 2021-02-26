7236d88839e6ebbc97ed98a273527bebb758bc96
Serega-555 <21zozo@mail.ru>
2021-02-27 00:19:33 +0600

    Рисует треугольники но не могу разместить все по центру

--- tes.py
+++ tes.py
@@ -2,18 +2,22 @@
 
 import turtle as t
 import math
+# R = pix / (2 * math.sin(math.radians(360 / (2 * n))))
 
-
-def square(n, pix):
-
-    for j in range(n):
+def square():
+    m = 5
+    n = 3
+    pix = 20
+    for i in range(m):
         R = pix / (2 * math.sin(math.radians(360 / (2 * n))))
         t.penup()
-        t.goto(-R, -R)
-        for jj in range(3, n + 1, 1):
+        t.goto(R, R)
+        print(R)
+        for j in range(n):
+
             t.pendown()
             t.forward(pix)
-            t.left(360 / jj)
-        
-
-square(10, 40)
+            t.left(360 / n)
+        n += 1
+        pix += 10
+square()
